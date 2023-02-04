706975e3c951f89b4abea4be7ddb953bee2fbb59
Mathias Bærentsen <matb@morningtrain.dk>
2023-02-04 23:13:35 +0100

    ✨ Add setup of location rule class

--- src/DatabaseModelAdminUi.php
+++ src/DatabaseModelAdminUi.php
@@ -3,17 +3,24 @@
 namespace Morningtrain\WP\DatabaseModelAdminUi;
 
 use Morningtrain\PHPLoader\Loader;
+use Morningtrain\WP\Hooks\Hook;
 use Morningtrain\WP\View\View;
 
 class DatabaseModelAdminUi
 {
 
+    private static array $modelsDir;
+
     public static function setup(string|array $modelsDir): void
     {
+        static::$modelsDir = (array) $modelsDir;
+
         Loader::create($modelsDir)
             ->call('setupAdminUi');
 
         View::addNamespace('wpdbmodeladminui', dirname(__DIR__) . '/resources/views');
+
+        static::setupAcf();
     }
 
     public static function getAdminPageUrlWithQueryArgs(string $page, int $modelId, ?string $action = null, ?string $nonce = null): string
@@ -27,6 +34,22 @@ class DatabaseModelAdminUi
             ],
             admin_url('admin.php')
         );
+    }
+
+    public static function getEloquentModelsDirs(): array
+    {
+        return static::$modelsDir;
+    }
+
+    private static function setupAcf(): void
+    {
+        if (! class_exists('ACF')) {
+            return;
+        }
+
+        Hook::action('acf/init', function () {
+            acf_register_location_type(AcfEloquentModelLocation::class);
+        });
     }
 
 }
