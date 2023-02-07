40d6e431cb5b37783d8c6657c359be4c22de9fc8
Mathias Bærentsen <matb@morningtrain.dk>
2023-02-07 23:34:31 +0100

    ✨ Add ModelPageAcfSettings class

---
+++ src/Classes/ModelPage/ModelPageAcfSettings.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Morningtrain\WP\DatabaseModelAdminUi\Classes\ModelPage;
+
+class ModelPageAcfSettings
+{
+
+    public array $extraLoadCallbacks = [];
+    public $extraSaveCallback = null;
+
+    public function withExtraLoadCallback(string $slug, callable|string $callback): self
+    {
+        $this->extraLoadCallbacks[$slug] = $callback;
+
+        return $this;
+    }
+
+    public function withExtraSaveCallback(callable|string $callback): self
+    {
+        $this->extraSaveCallback = $callback;
+
+        return $this;
+    }
+
+}
