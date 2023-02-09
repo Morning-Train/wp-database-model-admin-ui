8a00fa5f8abc7110113c61781c16505305b82de3
Mathias Bærentsen <matb@morningtrain.dk>
2023-02-09 22:39:30 +0100

    ✨ Add methods, to convert array with field keys to names and reverse

--- src/Classes/Helper.php
+++ src/Classes/Helper.php
@@ -2,8 +2,6 @@
 
 namespace Morningtrain\WP\DatabaseModelAdminUi\Classes;
 
-use Morningtrain\WP\DatabaseModelAdminUi\Classes\ModelPage\ModelPage;
-
 class Helper
 {
 
@@ -19,34 +17,71 @@ class Helper
         return static::$eloquentModelsDirs;
     }
 
-    public static function getCurrentModePageFromUrlPage(): ?ModelPage
+    public static function getAcfValuesWithNames(array $array): array
     {
-        $page = $_GET['page'] ?? null;
+        $newArray = [];
 
-        foreach (ModelPages::getModelPages() as $modelPage) {
-            if ($modelPage->acfEditable && $page === $modelPage->acfEditablePageSlug) {
-                return $modelPage;
+        foreach ($array as $key => $value) {
+            $field = get_field_object($key);
+
+            // Handle group
+            if (is_array($value) && str_starts_with(array_key_first($value), 'field_')) {
+                $value = static::getAcfValuesWithNames($value);
+
+                $newArray[$field['name']] = $value;
+                continue;
             }
 
-            if ($page === $modelPage->pageSlug) {
-                return $modelPage;
+            // Handle repeater
+            if (is_array($value) && ! str_starts_with(array_key_first($value), 'field_')) {
+                $newValue = [];
+                foreach ($value as $_value) {
+                    $newValue[] = static::getAcfValuesWithNames($_value);
+                }
+
+                $newArray[$field['name']] = $newValue;
+                continue;
             }
+
+
+            $newArray[$field['name']] = $value;
         }
 
-        return null;
+        return $newArray;
     }
 
-    public static function getCurrentModePageFromUrlAcfEditablePage(): ?ModelPage
+    public static function convertNamesToFieldKeys(array $array): array
     {
-        $page = $_GET['page'] ?? null;
+        $newArray = [];
 
-        foreach (ModelPages::getModelPages() as $modelPage) {
-            if ($modelPage->acfEditable && $page === $modelPage->acfEditablePageSlug) {
-                return $modelPage;
+        foreach ($array as $key => $value) {
+            if (is_array($value)) {
+                $newValue = [];
+
+                foreach ($value as $_key => $_value) {
+                    if (is_array($_value)) {
+                        $newValue[] = static::convertNamesToFieldKeys($_value);
+                        continue;
+                    }
+
+                    $_field = acf_get_field($_key);
+                    $newValue[$_field['key']] = $_value;
+                }
+
+                $newArray[] = $newValue;
+                continue;
             }
+
+            if (is_numeric($key)) {
+                $newArray[$key] = $value;
+                continue;
+            }
+
+            $field = acf_get_field($key);
+            $newArray[$field['key']] = $value;
         }
 
-        return null;
+        return $newArray;
     }
 
 }
